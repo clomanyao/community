fa2e2ec640ad4c285b543686512db07c33169610
2482118722ysj <2482118722@qq.com>
2019-11-20 16:42:24 +0800

    重新提交错误页面的访问处理问题

--- src/main/java/com/company/community/controller/error/MyCustomizeErrorController.java
+++ src/main/java/com/company/community/controller/error/MyCustomizeErrorController.java
@@ -3,13 +3,14 @@ package com.company.community.controller.error;
 import org.springframework.boot.autoconfigure.web.servlet.error.AbstractErrorController;
 import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 
-//@Controller
+@Controller
 public class MyCustomizeErrorController extends AbstractErrorController {
 
     public MyCustomizeErrorController(ErrorAttributes errorAttributes) {
