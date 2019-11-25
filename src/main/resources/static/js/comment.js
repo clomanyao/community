cac9b50bc0bfd5155c3888a64afe68707b01d326
2482118722ysj <2482118722@qq.com>
2019-11-25 09:21:35 +0800

    二级评论列表使用jquery技术在页面展示成功

--- src/main/resources/static/js/comment.js
+++ src/main/resources/static/js/comment.js
@@ -1,7 +1,7 @@
 /*
 * （一级和二级评论）提交评论以json数据向后台传参
 * */
-function comment(parentId,type,context) {
+function comment(parentId, type, context) {
 
     if (context.length == 0 || context == null) {
         alert("评论不能为空");
@@ -20,10 +20,10 @@ function comment(parentId,type,context) {
         success: function (data) {
             if (data == 200) {
                 window.location.reload();  //刷新页面
-                if(type==1){
+                if (type == 1) {
                     document.getElementById("comment_context").value = "";  //当页面刷新的时候，清空textarea的内容
-                }else {
-                    document.getElementById("second"+parentId).value = "";  //当页面刷新的时候,清空二级评论textarea的内容
+                } else {
+                    document.getElementById("second" + parentId).value = "";  //当页面刷新的时候,清空二级评论textarea的内容
                 }
                 // $("#comment_section").hide();
             } else {
@@ -39,7 +39,7 @@ function comment(parentId,type,context) {
 function postcomment() {
     var questionId = $("#question_id").val();
     var context = $("#comment_context").val();
-    comment(questionId,1,context);
+    comment(questionId, 1, context);
 }
 
 /*
@@ -47,8 +47,8 @@ function postcomment() {
 * */
 function secondcommment(e) {
     var dataId = e.getAttribute("data-second");
-    var replycontext = $("#second"+dataId).val();
-    comment(dataId,2,replycontext);
+    var replycontext = $("#second" + dataId).val();
+    comment(dataId, 2, replycontext);
 }
 
 
@@ -68,19 +68,84 @@ function collapseComment(e) {
     //拿到data-id的值,即一级评论的id
     var dataId = e.getAttribute("data-id");
     //根据data-id的值拼接为二级评论的id,拿到div的标签对象
-    var comment = $("#comment"+dataId);
-    var commentspan = $("#"+dataId);
+    var comment = $("#comment" + dataId);
+    var commentspan = $("#" + dataId);
 
-    if (e.getAttribute("data-collapse")!=null){
+    if (e.getAttribute("data-collapse") != null) {
         //折叠二级评论
         comment.removeClass("in");//给class删除一个样式
         commentspan.removeClass("active");  //也可以 e.classList.remove("active");e是标签
         e.removeAttribute("data-collapse");
-    }
-    else{
-        //展开二级评论
-        comment.addClass("in");//给class添加一个样式
-        commentspan.addClass("active"); //e.classList.add("active")
-        e.setAttribute("data-collapse","true");
+    } else {
+        var twolevel = $("#twolevel" + dataId);
+        //当twolevel下子元素只有不为1的时候，不在加载二级评论
+        if (twolevel.children().length != 1) {
+            //展开二级评论
+            comment.addClass("in");//给class添加一个样式
+            commentspan.addClass("active"); //e.classList.add("active")
+            e.setAttribute("data-collapse", "true");
+        } else {
+            $.getJSON("/comment/" + dataId, function (data) {
+                $.each(data.reverse(), function (index, commentDTO) {
+                    var mediacommentuser = $("<div/>", {
+                        "class": "media commentuser",
+                    })
+
+                    var medialeft = $("<div/>", {
+                        "class": "media-left",
+                    })
+
+                    var commentimg = $("<a/>", {
+                        "href": commentDTO.user.avatarUrl,
+                        "class": "commentimg",
+                    })
+
+                    var imgcss = $("<img/>", {
+                        "class": "media-object img-thumbnail imgcss",
+                        "src": commentDTO.user.avatarUrl
+                    })
+
+                    mediacommentuser.append(medialeft);
+                    medialeft.append(commentimg);
+                    commentimg.append(imgcss);
+
+                    //下方div
+                    var mediabody = $("<div/>", {
+                        "class": "media-body"
+                    })
+
+                    var commentname = $("<h6/>", {
+                        "class": "commentname",
+                    })
+
+                    var namecss = $("<span/>", {
+                        "class": "namecss",
+                        "html": commentDTO.user.name
+                    })
+
+                    var span2element = $("<span/>", {
+                        "html": commentDTO.context
+                    })
+
+                    var span3element = $("<span/>", {
+                        "class": "pull-right commenttime",
+                        "html": moment(commentDTO.gmtCreate).format("YYYY-MM-DD")
+                    })
+
+                    mediacommentuser.append(mediabody);
+                    mediabody.append(commentname);
+                    commentname.append(namecss);
+                    mediabody.append(span2element);
+                    mediabody.append(span3element);
+
+                    twolevel.prepend(mediacommentuser);  //在mediacommentuser之前插入twolevel
+                });
+
+            });
+            //展开二级评论
+            comment.addClass("in");//给class添加一个样式
+            commentspan.addClass("active"); //e.classList.add("active")
+            e.setAttribute("data-collapse", "true");
+        }
     }
 }
